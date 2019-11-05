3fe05f02b7c6b536140856f930a4e11edd9c0c6a
James Grantham <james.r.grantham@gmail.com>
2019-11-05 14:50:49 +0000

    API request

--- cars/car-router.js
+++ cars/car-router.js
@@ -4,4 +4,14 @@ const carsRouter = express.Router()
 
 console.log(process.env.NODE_ENV)
 
+carsRouter.get('/', (req, res) => {
+  carsdb('cars')
+    .then(cars => {
+      res.json(cars)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to load cars' })
+    })
+})
+
 module.exports = carsRouter
--- data/migrations/20191105141309_cars-table.js
+++ data/migrations/20191105141309_cars-table.js
@@ -2,10 +2,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cars', table => {
     table.increments('Reference')
-    table.number('VIN').unique().notNullable()
+    table.decimal('VIN').unique().notNullable()
     table.text('Make').notNullable()
     table.text('Model').notNullable()
-    table.number('Mileage').notNullable()
+    table.decimal('Mileage').notNullable()
     table.boolean('Clean')
     table.boolean('Scrap')
   })
